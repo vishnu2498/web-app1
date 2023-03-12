568c8b535d2e2df7c32e9fabb799f2532f47be12
vishnu2498 <vishnuprakashstbasil@gmail.com>
2023-03-12 16:56:46 +0530

    first commit

---
+++ src/main/java/com/example/webapp1/WebApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.webapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webapp1/WebApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
