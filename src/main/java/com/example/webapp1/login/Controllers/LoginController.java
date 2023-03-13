69175edfb5585d2f6ac6bb309dffaf22dc3653ca
vishnu2498 <vishnuprakashstbasil@gmail.com>
2023-03-13 11:04:06 +0530

    update login controller

--- src/main/java/com/example/webapp1/login/Controllers/LoginController.java
+++ src/main/java/com/example/webapp1/login/Controllers/LoginController.java
@@ -1,8 +1,10 @@
 package com.example.webapp1.login.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,8 +12,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 
     @GetMapping("/jsp")
-    public String getLoginJsp()
+    public String getLoginJsp(@RequestParam String name, ModelMap model)
     {
+        model.put("name", name);
         return "login";
     }
 
